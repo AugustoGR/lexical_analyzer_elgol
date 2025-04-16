1d868a47e1fe43a6dbbbd2d7897ce22fc47ffbd0
Augusto <augusto.garciadarosa@gmail.com>
2025-04-15 23:32:15 -0300

    feat: Terminando implementação do analisador elgol

--- main.py
+++ main.py
@@ -1,47 +1,82 @@
 import sys
 import re
+from tabulate import tabulate
 
 # para rodar basta executar o arquivo main.py passando o arquivo de entrada como argumento
 # python main.py <arquivo_de_entrada>
-file = open(sys.argv[1], 'r')
+if sys.argv[1]:
+    file = open(sys.argv[1], 'r')
+else:
+    file = open('code.txt', 'r')
 
-symbols = [
-    ('int', 'reservada'),
-    ('float', 'reservada'),
-    ('double', 'reservada'),
-    ('char', 'reservada'),
-    ('if', 'reservada'),
-    ('while', 'reservada'),
-    ('for', 'reservada'),
-]
 
-tokensList = []
+
+reservedWords: list[str] = ['int', 'float', 'double', 'char', 'if', 'while', 'for']
+
+tableSymbols: list[tuple[str, str, str]] = []
+
+tokensList: list[str] = []
+
+def register_token(token, tipo):
+    newEntrada = len(tableSymbols) + 1
+    tableSymbols.append((newEntrada, token, tipo))
+    tokensList.append(f'<{tipo},{newEntrada}>')
 
 for line in file:
     line = line.strip()
 
-    if len(line) == 0 or line.startswith('#'):
+    if len(line) == 0 or re.match(r'^#', line):
         continue
-    tokens = re.split(r'\s+', line)
+    tokens =  re.findall(r'\d+|[a-zA-Z_]\w*|[()\[\]{}]|==|!=|<=|>=|[+\-*/=<>]', line)
     
     for token in tokens:
+
+        #region VERIFICAR SE O TOKEN TEM #
+        if re.match(r'^#', token):
+            break
+        #endregion VERIFICAR SE O TOKEN TEM #
+
+        #region VERIFICAR SE O TOKEN JÁ ESTÁ NA TABELA DE SÍMBOLOS. SE ESTIVER, ADICIONAR NA LISTA DE TOKENS
+        matched = False
+        for symbol in tableSymbols:
+            if token == symbol[1]:
+                tokensList.append(f'<{symbol[2]},{symbol[0]}>')
+                matched = True
+                break
+        if matched:
+            continue
+        #endregion VERIFICAR SE O TOKEN JÁ ESTÁ NA TABELA DE SÍMBOLOS. SE ESTIVER, ADICIONAR NA LISTA DE TOKENS
+
+        #region VERIFICAR SE O TOKEN É UM IDENTIFICADOR
         if re.match(r'^[A-Z][a-zA-Z]*$', token):
-            symbols.append((token, 'identificador'))
-            tokensList.append(('identificador', len(symbols) - 1))
+            register_token(token, 'identificador')
+        #endregion VERIFICAR SE O TOKEN É UM IDENTIFICADOR
+
+        #region VERIFICAR SE O TOKEN É UMA PALAVRA RESERVADA
         elif re.match(r'^[a-z][a-zA-Z]*$', token):
-            if token in [symbol[0] for symbol in symbols]:
-                idx = symbols.index((token, 'reservada'))
-                tokensList.append(('reservada', idx))
+
+            if token in reservedWords:
+                register_token(token, 'reservada')
+        #endregion VERIFICAR SE O TOKEN É UMA PALAVRA RESERVADA
+
+        #region VERIFICAR SE O TOKEN É UM NÚMERO
         elif re.match(r'^[0-9]+,?[0-9]+$', token):
-            symbols.append((token, 'numero'))
-            tokensList.append(('numero', len(symbols) - 1))
+            register_token(token, 'número')
+        #endregion VERIFICAR SE O TOKEN É UM NÚMERO
+
+        #region VERIFICAR SE O TOKEN É UM OPERADOR OU PONTUAÇÃO
         elif re.match(r'^[\(\);+\-%\*\[\]=.<>]$', token):
-            tokensList.append((token, '-'))
+            tokensList.append(f'<{token},>')
+        #endregion VERIFICAR SE O TOKEN É UM OPERADOR OU PONTUAÇÃO
 
-print('Tokens:', tokensList)
+print('.:TABELA DE SÍMBOLOS:.')
+print(tabulate(tableSymbols, headers=["ID", "Token", "Tipo"], tablefmt="grid"))
+print('\n.:LISTA DE TOKENS:.')
+for token in tokensList:
+    print(token)
 
-print('Simbolos:', symbols)
-
+with open('tokens_output.txt', 'w') as output_file:
+    output_file.write(' '.join(tokensList))
 
 # Ainda é preciso ajusta a leitura de pontuação colados ao texto "int a = 0;"
 # vericar se todos os tokens estão sendo lidos corretamente
